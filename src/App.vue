7ab0d784de687404b6c896d843bcd8e4f07c2244
Gabriel Morais <56984939+mogba@users.noreply.github.com>
2021-12-08 08:11:07 -0300

    Início da criação da página principal

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <div id="app">
-      <button class="button is-primary">This is a button</button>
-  </div>
+    <div id="app">
+        <head></head>
+        <body>
+            <main-page />
+        </body>
+    </div>
 </template>
 
 <script>
+import MainPage from "./components/MainPageComponent.vue";
+
 export default {
-  name: 'App',
+    name: 'App',
+    components: {
+        MainPage,
+    }
 }
 </script>
 
---
+++ src/components/MainPageComponent.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="/">
+                        <img src="../assets/sinapse-logo.png" alt="Logo Sinapse">
+                    </a>
+                </div>
+                <div id="navbarMenu" class="navbar-menu">
+                    <div class="navbar-end">
+                        <!-- <a class="navbar-item is-active">
+                        Home
+                        </a>
+                        <a class="navbar-item">
+                        Examples
+                        </a> -->
+                        <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                            Últimas sinapses visitadas
+                        </a>
+                        <div class="navbar-dropdown is-boxed">
+                            <a class="navbar-item" href="admin.html">
+                                Admin
+                            </a>
+                            <a class="navbar-item" href="forum.html">
+                                Forum
+                            </a>
+                            <a class="navbar-item" href="cover.html">
+                                Cover
+                            </a>
+                            <a class="navbar-item" href="cards.html">
+                                Cards
+                            </a>
+                            <a class="navbar-item" href="blog.html">
+                                Blog
+                            </a>
+                            <hr class="navbar-divider">
+                            <a class="navbar-item" href="search.html">
+                                Search
+                            </a>
+                            <a class="navbar-item is-active" href="kanban.html">
+                                Kanban
+                            </a>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="section">
+                <div class="columns">
+                    <div class="column is-centered">
+                        <div class="control has-icons-left">
+                            <input class="input" type="search" placeholder="Digite para pesquisar">
+                            <span class="icon is-small is-left">
+                                <font-awesome-icon icon="search" />
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainPage',
+}
+</script>
